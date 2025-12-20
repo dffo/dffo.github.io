fa0deddb09341985d57e776b25ffd3dd42ec4c18
Derek Furtado <furtadod@mcmaster.ca>
2025-12-20 17:39:36 -0500

    update

---
+++ politics/public_animal_ethic.html.pm
@@ -0,0 +1,177 @@
+#lang pollen
+◊(define-meta doc-publish-date "2025-11-08")
+
+◊(title "A consistent public ethic for animal welfare" 
+"The Gratuitous Harm Principle and its alternatives" #:big? #f)
+◊(print-date)
+
+◊initial-cap{The purpose of} this inquiry is to assess the most consistent
+rule that can be used to set public policy towards animal welfare.
+While individuals may remain free to take even further action according
+to their private conscience, a public ethic should seek to avoid perverse
+incentives and moral hazards while consistently guaranteeing some baseline
+of animal welfare.
+
+
+We start with two fundamental axioms that define the system and objective,
+and then assess four different philosophical approaches (P1--P4) in terms of how
+successfully they operationalize the axioms, in descending order of absolutism.
+The inquiry is empirical in nature
+rather than deductive: it evaluates each rule against real-world cases,
+rejecting those whose results violate the axioms. The rule that survives this process
+of falsification is then adopted as the most consistent public ethic.
+
+◊b{Axiom 1 (A1).} All life has some intrinsic worth, whether human or not.
+
+◊b{Implication.} 
+This blocks the degenerate solution of extinguishing all life to forever 
+minimize pain,
+since no life means no suffering. It applies both universally and in microcosm,
+barring solutions that implicitly replace a farm or garden with barren land devoid
+of life. Any tradeoffs must be compared not with a null baseline, but a living
+one.
+
+◊b{Axiom 2 (A2).} The public should seek to minimize the ◊i{total harm} to all
+animals across all contexts, whether caused by humans or otherwise.
+
+◊b{Note.} Total harm is inherently unknowable and unquantifiable in general.
+If this were not the case, the inquiry itself would be unnecessary, as we
+would simply choose our actions to optimize total harm directly.
+Instead, each of the following propositions is tested as a rule that allows
+us to minimize total harm without having to quantify it; we evaluate each
+by identifying constrained, tractable cases that can reveal directional changes
+in harm. This lets us test consistency with the axioms without needing to
+measure the unknowable.
+
+◊b{Proposition 1 (P1).} Slaughter is intrinsically wrong, regardless of context or
+outcome.
+
+◊b{Analysis.} P1 aligns with ◊i{deontological} approaches in animal
+ethics. But if all slaughter is wrong, it forbids us from culling an invasive
+species, even if the resulting
+overpopulation results in the destruction of ecosystems and the species' own
+eventual death. 
+If we had successfully culled them, the invasive species still would
+have died, but the surrounding ecosystem's species would not. Thus P1's
+lack of intervention must increase total suffering over inaction. 
+A similar quandary exists for animals with no remaining natural predators (i.e.
+species like wolves made extinct by humans).
+
+◊b{Conclusion.} P1 is rejected as inconsistent with A2.
+
+◊b{Proposition 2 (P2).} Causing unnecessary suffering to animals is wrong.
+
+◊b{Analysis.} P2 aligns with many pragmatic liberal traditions in animal ethics.
+The problem is that ◊i{unnecessary} is undefined and its meaning can only
+derive from an ◊i{a priori} value judgment. If ◊i{unnecessary} is defined directly
+in terms of total harm, then P2 simply restates A2,
+thus begging the question. As noted in the discussion for A2, such a "rule"
+is unusable because total harm is impossible to directly measure---the very
+purpose of a rule is to approximate it.
+
+If instead ◊i{unnecessary} is defined differently,
+for example in vegan approaches where direct consumption is proscribed but 
+incidental death ignored, it imports an extraneous standard that may not
+minimize total harm in all cases: for example, invasive-species hunting may
+be the best way to incentivize the killing of animals that would otherwise
+destroy ecosystems, thus averting a much greater total harm to animals.
+Thus veganism and similar ◊i{a priori} standards fail the same test as P1.
+
+◊b{Conclusion.} P2 is rejected as either circular or inconsistent with A2.
+
+◊b{Proposition 3 (P3).} Humans owe a ◊i{duty of care} to animals they raise, 
+obliging them to minimize the suffering of domestic animals in life and death.
+
+◊b{Analysis.} P3 reflects a stewardship-based approach to animal ethics.
+But while intuitively appealing, it leads to perverse ethical incentives that
+could ironically lead to an increase in total harm across all contexts (as 
+defined in A2). 
+
+Animals can suffer in both nature and captivity. If animal
+suffering in captivity is ethically penalized beyond animal suffering in
+nature, it would be more ethical to hunt game that spent most of its life
+afflicted with hunger and disease than to slaughter and eat an animal that was
+raised in better conditions. The cause is an asymmetry in ethical cost,
+which means that the optimal decision under P3 increases total
+harm as defined by A2.
+
+◊b{Conclusion.} P3 is rejected as distortionary and inconsistent with A2.
+
+◊b{Proposition 4 (P4).} Humans must avoid ◊i{gratuitous} cruelty to animals,
+i.e. cruelty beyond what nature would impose if said 
+animals existed in nature untouched by humans
+(a ◊i{natural baseline}), and beyond what can be avoided 
+costlessly, i.e.
+without introducing
+distortionary effects or perverse incentives. 
+Additional kindness is left to personal conscience.
+
+◊b{Analysis.} P4 is a conservative and local optimizer that always
+guarantees a reduction in total harm compared with the absence of any
+principle (which
+would permit any measure of sadism), but
+does not guarantee the 
+maximum possible 
+reduction. All methods of torture or maltreatment that can be removed without
+cost are proscribed.
+The natural
+baseline also prevents the defects found in P1 and P2: an invasive species, 
+when subjected to a natural baseline, would require human beings to cull it
+as a stand-in for nature.
+Similarly, if we drive a natural predator such as wolves to extinction,
+we must take their place to prevent overpopulation by culling their prey
+(e.g. deer) as needed.
+
+Vegans may object that this permits the direct consumption of animals, which
+they might argue is inherently unethical.
+But in cases where human activity causes animal harm, such as the slaughter of
+livestock for food, it is impossible to know if the absence of such activity
+would itself reduce total harm. 
+For example, a farm that is abandoned may be
+replaced with a forest where many more animals die of hunger and 
+predation. The only certain way to
+reduce suffering beyond this is to replace both the farm and forest with 
+barren land devoid of life. But that violates A1, as it degrades the world's
+capacity to sustain life in order to reduce harm.
+
+P4 therefore treats livestock symmetrically to wild animals, reflecting how
+A2 applies to both human- and nature-caused harm equally.
+It implies that
+humans are to livestock as nature is to wild animals, and so our
+treatment of our animals should be no worse than nature's treatment of its 
+own. Indeed, we should ◊i{improve} the treatment of animals where we can
+without cost, which establishes a kind of ethical ratchet. The floor is nature,
+but our treatment of them must improve as our abilities grow. 
+Nature is not treated as moral license, but as
+an operationally stable and epistemically neutral benchmark.
+
+P4 only requires ◊i{costless} improvements because costly improvements could
+create perverse incentives that increase total harm, as in P3.
+Costless changes by definition do not affect consumption patterns, thus
+guaranteeing a reduction in total harm without second-order effects.
+So long as we treat livestock at least as well as wild
+animals in nature 
+and better where possible (i.e. medicine, food, fraction of total lifespan),
+such treatment complies with both P4 and A2. 
+
+◊;{}
+
+◊b{Conclusion.} P4 is conservative but accepted as consistent with A2.
+
+What allows the inquiry to work despite
+the apparently enormous burden of A2 is the use of epistemic constraints.
+Though a blanket goal of reducing harm would seem to require
+us to micromanage and centrally plan nature,
+the complexity of such an approach means that we cannot know if it
+would actually reduce total harm compared with inaction.
+Thus we are not forced into fanciful schemes that could lead to disaster.
+
+P4 is therefore the most consistent public ethic for animal welfare---one 
+that always guarantees an improvement in total welfare, while giving people
+freedom to go further in line with their conscience. It is also compatible
+with most religious and traditional approaches to animal welfare. P4 may
+be referred to as the ◊i{gratuitous harm principle}, which this inquiry 
+finds to be the best ethical approach for public policy.
+◊end-mark{}
+
+
